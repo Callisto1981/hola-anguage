937f86a292b01d43e0d1e7df294e4a12dbd6c408
Wayne <wabbruscatoorr@gmail.com>
2019-10-07 22:22:18 -0400

    Initial commit

---
+++ js/scripts.js
@@ -0,0 +1,31 @@
+(function showModal(/*title, content, dog-image*/) {
+  var $modalContainer = $('#modal-container');
+  $modalContainer.addClass('is-visible');
+  $modalContainer.innerHTML = '';
+
+  var $modal = $('div');
+  $modal.addClass('modal');
+
+  var $closeButtonElement = $('<button class='modal-close'>'Close</div>');
+  $('click').append($closeButtonElement);
+  /*$closeButtonElement.addClass('modal-close');
+  $closeButtonElement.innerText = 'Close';
+  $closeButtonElement.$('click', hideModal);*/
+
+  var $nameElement = $('<h1>title</h1>');
+  //$nameElement.innerText = title;
+
+  var $heightElement = $('<p>height</p>');
+  //$heightElement.innerText =height;
+
+  var $imageElement = $('<img src=''>');
+  //$imageElement.src = /*dog-image*/;
+
+  $modal.append($closeButtonElement);
+  $modal.append($nameElement);
+  $modal.append($heightElement);
+  $modal.append($imageElement);
+  $modalContainer.append(modal);
+
+  $modalContainer.add('is-visible');
+});
